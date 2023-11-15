fcb6e0f6792a72df587d9f281162d23d48907d6b
RodaAlexis <83518953+RodaAlexis@users.noreply.github.com>
2023-11-14 23:51:11 -0300

    Update13 settings.py

--- stock_finalposta/stock_nuevo/control/control/settings.py
+++ stock_finalposta/stock_nuevo/control/control/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'control.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/control',
-        conn_max_age=600
-    )
+    'default': {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 database_url = os.environ.get("DATABASE_URL")
